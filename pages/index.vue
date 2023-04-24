1276eec9a3f916469dc97b7bc0f92e763491daff
yc97463 <hi@imych.one>
2023-04-25 02:20:02 +0800

    fix(pages/index): fix typo

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <h1 class="text-center font-medium text-2xl">請選擇維修類別</h1>
     <div class="">
       <RepairChoosenCard icon="building" title="校舍宿舍報修" description="總務處校舍、宿舍維修相關通報。" action="apply"
-        url="/auth/login?redirec=/repair" />
+        url="/auth/login?return=/repair" />
       <RepairChoosenCard icon="wifi-issue" title="網路異常報修" description="圖資中心宿舍網路斷線、異常報修。" action="go"
         url="http://134.208.12.37/repair/" />
       <RepairChoosenCard icon="wrench" title="教學設備報修" description="教學卓越中心教室、教學設備報修。" action="go"
