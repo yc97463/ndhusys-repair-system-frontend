8779efdf7b1d9a5b664185369694152717d9cc22
yc97463 <hi@imych.one>
2023-04-25 00:14:35 +0800

    feat(components/FooterBadge): add Information Badge

---
+++ components/FooterBadge.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="my-3 mr-5">
+    <span class="px-4 py-1.5 text-sm bg-white rounded-full shadow-sm">{{ title }}</span>
+    <span class="ml-2">{{ content }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    content: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
