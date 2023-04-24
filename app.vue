f3d4eaaa87c77a16f004da69af995f7e29e33ded
yc97463 <hi@imych.one>
2023-04-24 19:23:49 +0800

    init components, layouts, pages

--- app.vue
+++ app.vue
@@ -1,7 +1,17 @@
-<template>
+<!-- <template>
   <div>
     <span class="text-3xl font-bold underline">
       Hello world!
     </span>
+    <AppAlert>
+      This is a message from the parent component.
+    </AppAlert>
+  </div>
+</template> -->
+
+<template>
+  <div>
+    <!-- Markup shared across all pages, ex: NavBar -->
+    <NuxtPage />
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Welcome to the homepage</h1>
+    <AppAlert>
+      This is an auto-imported component
+    </AppAlert>
+    <button class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">...</button>
+
+  </div>
+</template>
