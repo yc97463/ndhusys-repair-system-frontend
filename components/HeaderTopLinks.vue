7b0e85178af09d1e45c1fec106592ef36f92eb56
yc97463 <hi@imych.one>
2023-04-25 00:18:39 +0800

    feat(components/HeaderTopLinks): init top header links

---
+++ components/HeaderTopLinks.vue
@@ -0,0 +1,20 @@
+<template>
+  <a :href="url" target=" _blank" class="text-white pl-3.5" rel="noopener noreferrer">
+    {{ name }}
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    url: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
