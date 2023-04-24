e22c4756549dd8ae75c27379cb58e1ba7848ba91
yc97463 <hi@imych.one>
2023-04-25 00:19:05 +0800

    feat(components/NavLinks): init links in navbar

---
+++ components/NavLinks.vue
@@ -0,0 +1,21 @@
+<template>
+  <NuxtLink :to="url" class="pl-3.5" rel="noopener noreferrer">
+    {{ name }}
+  </NuxtLink>
+</template>
+
+<script>
+
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    url: {
+      type: String,
+      required: true
+    }
+  },
+}
+</script>
